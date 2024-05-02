9977abda8a8aa38ec1678abd72c97bd7d2b0a9bc
jborkows <jacek.borkowskiit@gmail.com>
2024-05-02 12:29:39 +0200

    pcall is boolean, result

--- lua/gotest/core/tschecker.lua
+++ lua/gotest/core/tschecker.lua
@@ -3,12 +3,6 @@ local function is_plugin_installed(name)
 	local present, _ = pcall(require, name)
 	return present
 end
--- Function to check if TS grammar for a language is installed
-local function is_ts_language_installed(lang)
-	local ts = require("nvim-treesitter")
-	local installed = ts.is_installed(lang)
-	return installed
-end
 ---checks if Treesitter language is installed and installs it if it is not there
 ---@param languageName string
 M.checkLanguage = function(languageName)
@@ -17,8 +11,6 @@ M.checkLanguage = function(languageName)
 		return
 	end
 
-	if not is_ts_language_installed(languageName) then
-		vim.api.nvim_command("TSInstall " .. languageName)
-	end
+	vim.api.nvim_command("TSUpdate " .. languageName)
 end
 return M
